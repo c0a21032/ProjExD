54129a7ae3fb5f37a95dcab5996c0a3fb23c9525
katoren <c0a210329b@edu.teu.ac.jp>
2022-06-21 14:28:43 +0900

    二問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,4 +5,13 @@ if __name__ == "__main__":
     root.title("計算機")
     root.geometry("300x500")
 
+    r, c = 0, 0
+    for num in range(9, -1, -1):
+        button = tk.Button(root, width=4, height=2, text=str(num), font=("Times New Roman", 30))
+        button.grid(row=r, column=c)
+        c += 1
+        if (num-1)%3 == 0:
+            r += 1
+            c = 0
+
     root.mainloop()
