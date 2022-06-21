8c1bfbb1ad3a2f5aea78f42c4436f09511871dce
katoren <c0a210329b@edu.teu.ac.jp>
2022-06-21 14:53:18 +0900

    六問目

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,7 +9,7 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("計算機")
-    root.geometry("300x500")
+    #root.geometry("300x500")
 
     entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
     entry.grid(row=0, column = 0, columnspan=3)
@@ -21,5 +21,9 @@ if __name__ == "__main__":
         button.grid(row=r, column=num%3)
         if (num-1)%3 == 0:
             r += 1
+    
+    button = tk.Button(root, width=4, height=2, text="+", font=("Times New Roman", 30))
+    button.bind("<1>", button_click)
+    button.grid(row=4, column=1)
 
     root.mainloop()
