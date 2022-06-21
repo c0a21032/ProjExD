56ae243897cf448de277675099eb3e67d7345439
katoren <c0a210329b@edu.teu.ac.jp>
2022-06-21 16:05:20 +0900

    四則演算

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,6 +10,8 @@ def button_click(event):
         res = eval(eqn)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif num == "clear":
+        entry.delete(0, tk.END)
     else:
         entry.insert(tk.END, num)
 
@@ -20,20 +22,26 @@ if __name__ == "__main__":
     #root.geometry("300x500")
 
     entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-    entry.grid(row=0, column = 0, columnspan=3)
+    entry.grid(row=0, column = 0, columnspan=4)
 
-    r, c = 1, 0
-    for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
-        button = tk.Button(root, width=4, height=2, text=str(num), font=("Times New Roman", 30))
+    r, c = 4, 1
+    for i in range(10):
+        button = tk.Button(root, width=4, height=2, text=i, font=("Times New Roman", 30))
         button.bind("<1>", button_click)
         button.grid(row=r, column=c)
         c += 1
-        if (i+1)%3 == 0:
-            r += 1
+        if i%3 == 0:
+            r -= 1
             c = 0
     
-    button = tk.Button(root, width=4, height=2, text="+", font=("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.grid(row=4, column=1)
+    for i, num in enumerate(["+", "-", "*", "/"]):
+        button = tk.Button(root, width=4, height=2, text=num, font=("Times New Roman", 30))
+        button.bind("<1>",button_click)
+        button.grid(row=i+1, column=3)
+
+    for i, num in enumerate(["clear", "="]):
+        button = tk.Button(root, width=4, height=2, text=num, font=("Times New Roman", 30)) 
+        button.bind("<1>",button_click)
+        button.grid(row=4, column=i*2)
 
     root.mainloop()
