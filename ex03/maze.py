8c809cf75c9ffeeb8e3e0b5c257bcb8b81f7b937
katoren <c0a210329b@edu.teu.ac.jp>
2022-06-28 17:07:28 +0900

    ４限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
+import enum
+from itertools import count
 import tkinter as tk
+import tkinter.messagebox as tkm
 import maze_maker
+import random
 
 def key_down(event):
     global key
@@ -10,7 +14,7 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy, mx, my
+    global mcx, mcy, mx, my
     if key == "Up" and meiro[my-1][mx] == 0:
         my -= 1
     elif key == "Down" and meiro[my+1][mx] == 0:
@@ -19,11 +23,42 @@ def main_proc():
         mx -= 1
     elif key == "Right" and meiro[my][mx+1] == 0:
         mx += 1
-    cx = 50 + 100 * mx
-    cy = 50 + 100 * my
-    canvas.coords("tori", cx, cy)
+    mcx = 50 + 100 * mx
+    mcy = 50 + 100 * my
+    canvas.coords("tori", mcx, mcy)
     canvas.pack()
     root.after(100, main_proc)
+
+def enemy():
+    global ecx, ecy, ex, ey
+    delta = random.randint(1, 4)
+    if delta == 1 and ey-1 > 0:
+        ey -= 1
+    elif delta == 2 and ey+1 < 9:
+        ey += 1
+    elif delta == 3 and ex-1 > 0:
+        ex -= 1
+    elif delta == 4 and ex+1 < 14:
+        ex += 1
+    ecx = 50 + 100 * ex
+    ecy = 50 + 100 * ey
+    canvas.coords("teki", ecx, ecy)
+    canvas.pack()
+    root.after(500, enemy)
+
+def dstar():
+    for s in stars:
+        canvas.create_image(50 + 100 * s[0], 50 + 100 * s[1], image=star)
+        canvas.pack()
+    root.after(100, dstar)
+
+def isclear():
+    global stars
+    if (mx, my) in stars:
+        stars.remove((mx, my))
+    if (mx, my) == (ex, ey) and len(stars)==0:
+        tkm.showinfo("Congratulations!!", "クリアです！おめでとうございます！")
+    root.after(100, isclear)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -35,10 +70,29 @@ if __name__ == "__main__":
     meiro = maze_maker.make_maze(15, 9)
     maze_maker.show_maze(canvas, meiro)
 
+    stars = []
+    star = tk.PhotoImage(file="ex03/fig/5.png")
+    while len(stars) < 8:
+        sx = random.randint(0, 14)
+        sy = random.randint(0, 8)
+        if meiro[sy][sx] == 0 and (sx, sy) not in stars:
+            stars.append((sx, sy))
+
     mx, my = 1, 1
-    cx, cy = 50 + 100 * mx, 50 + 100 * my
+    mcx, mcy = 50 + 100 * mx, 50 + 100 * my
     tori = tk.PhotoImage(file="ex03/fig/2.png")
-    canvas.create_image(cx, cy, image=tori, tag="tori")
+    canvas.create_image(mcx, mcy, image=tori, tag="tori")
+    canvas.pack()
+
+    ex, ey = 0, 0
+    while True:
+        ex = random.randint(7, 14)
+        ey = random.randint(4, 8)
+        if meiro[ey][ex] == 0:
+            break
+    ecx, ecy = 50 + 100 * ex, 50 + 100 * ey
+    teki = tk.PhotoImage(file="ex03/fig/6.png")
+    canvas.create_image(ecx, ecy, image=teki, tag="teki")
     canvas.pack()
 
     key = ""
@@ -46,4 +100,7 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
+    enemy()
+    isclear()
+    dstar()
     root.mainloop()
