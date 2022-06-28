df896361c766c3bd850ba3a4492d23e6608a1875
katoren <c0a210329b@edu.teu.ac.jp>
2022-06-28 14:03:31 +0900

    3問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="ex03/fig/2.png")
     canvas = tk.Canvas(width="1500", height="900", bg="black")
+    canvas.create_image(cx, cy, image=tori, tag="tori")
     canvas.pack()
 
     root.mainloop()
