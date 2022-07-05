b3e3e0d2a8b7e0c08895b9ee121c5f46281bd4b2
katoren <c0a210329b@edu.teu.ac.jp>
2022-07-05 13:37:50 +0900

    2問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,6 +6,13 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     bg = pg.image.load("ex04/pg_bg.jpg")
     bg_rect = bg.get_rect()
+    while True:
+        screen.blit(bg, bg_rect)
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        pg.display.update()
+        clock = pg.time.Clock()
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
